32ee4b60aa7905a99a355b547773a1677bb279fb
DBYannis <yannis.denantboemont@hotmail.com>
2023-07-31 09:22:43 +0200

    Navigation, beginning of login screen

--- App.js
+++ App.js
@@ -1,14 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import LoginScreen from './screens/LoginScreen';
+import SubscriptionScreen from './screens/SubscriptionScreen';
+import UserScreen from './screens/UserScreen';
+
+
+const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+
+const TabNavigator = () => {
+ return (
+   <Tab.Navigator screenOptions={{ headerShown: false }}>
+     <Tab.Screen name="Home" component={SubscriptionScreen} />
+     <Tab.Screen name="User" component={UserScreen} />
+     <Tab.Screen name="Favorite" component={LoginScreen} />
+   </Tab.Navigator>
+ );
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="TabNavigator" component={TabNavigator} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
-}
+ }
 
 const styles = StyleSheet.create({
   container: {
--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,9 +1,59 @@
-import {View } from 'react-native';
+import {View, Text, TextInput, TouchableOpacity, Image, StyleSheet, Button } from 'react-native';
 
-export default function LoginScreen(){
+export default function LoginScreen({ navigation }){
 return(
-<View>
-
+<View style={styles.container}> 
+    <View style={styles.logoContainer}>
+        <Image source={require('../assets/logo.png')} style={styles.logoImg}></Image>
+        <Text style={styles.logoTitle}>DrugSync</Text>
+    </View>
+    <View style={styles.inputContainer}>
+        <TextInput placeholder='Email'></TextInput>
+        <TextInput placeholder='Password'></TextInput>
+    </View>
+    <View style={styles.btnContainer}>
+        <Button
+            title="Login"
+            onPress={() => navigation.navigate('TabNavigator')}
+        />
+        <Button
+            title="Signup"
+       />
+    </View>
 </View>
 )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+      },
+      logoContainer: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginBottom: 200,
+      },
+      btnContainer: {
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+      },
+      inputContainer: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginBottom: 10,
+      },
+    logoTitle: {
+      fontFamily: 'Roboto',
+      fontSize: 28,
+      color: '#5DA6A0',
+    },
+    logoImg: {
+        width: 173,
+        height: 180,
+      },
+  });
